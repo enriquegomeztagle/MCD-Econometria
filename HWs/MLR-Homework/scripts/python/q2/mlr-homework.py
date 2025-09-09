b9239d1c1d3aaef6a17fefbf8810790e59bfeeeb
enriquegomeztagle <0241823@up.edu.mx>
2025-09-09 01:24:35 -0600

    Refactor mlr-homework.py by removing unused imports and functions
    - Eliminated unnecessary imports and functions related to saving plots and exporting LaTeX tables to streamline the script.
    - Simplified the code structure to focus on the core analysis functionality, enhancing readability and maintainability.

--- HWs/MLR-Homework/scripts/python/q2/mlr-homework.py
+++ HWs/MLR-Homework/scripts/python/q2/mlr-homework.py
@@ -8,54 +8,6 @@ from scipy import stats
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
-import os
-from pathlib import Path
-
-# %%
-DATA_DIR = Path("../../data/")
-DATA_DIR.mkdir(parents=True, exist_ok=True)
-
-LATEX_OUT = Path("../../docs/latex_utils/tables")
-LATEX_OUT.mkdir(parents=True, exist_ok=True)
-
-
-# %%
-def save_plot(
-    plot: plt.Figure,
-    filename: str,
-    format: str = "png",
-    dpi: int = 300,
-    close: bool = True,
-):
-    PLOTS_DIR.mkdir(parents=True, exist_ok=True)
-    filepath = PLOTS_DIR / f"{filename}.{format}"
-    try:
-        plot.savefig(filepath, format=format, dpi=dpi, bbox_inches="tight")
-        if close:
-            plt.close(plot)
-        print(
-            f"\nPlot {filename}.{format} saved correctly in {PLOTS_DIR}/{filename}.{format}"
-        )
-    except Exception as e:
-        print(f"\nCould not save plot {filename}.{format}. Reason: {e}")
-
-
-# %%
-def save_latex_table(df, filename: str, rename_map: dict, caption: str, label: str):
-    try:
-        table_tex = df.rename(columns=rename_map).to_latex(
-            index=False,
-            float_format="%.4f",
-            caption=caption,
-            label=label,
-        )
-        with open(LATEX_OUT / filename, "w") as f:
-            f.write(table_tex)
-        print(f"\nFile {filename} exported correctly in {LATEX_OUT}/{filename}")
-    except Exception as e:
-        print(f"\nCould not export {filename}. Reason: {e}")
-
-
 # %%
 data = pd.DataFrame(
     {
